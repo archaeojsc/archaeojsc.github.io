a8bc16d25e5a39ce4a3d347548ce6fe1c293b5b6
J. Scott Cardinal <archaeo_scott@yahoo.com>
2022-10-07 16:41:11 -0400

    fixed sim functions, added TOM

--- docs/assemblage_network/assemblage_networks_bipartite3.R
+++ docs/assemblage_network/assemblage_networks_bipartite3.R
@@ -1,8 +1,6 @@
 require(tidyverse)
 require(igraph)
 require(ggraph)
-require(ade4) # for jaccard similarity index
-require(lsa) #for cosine similarity index
 
 # Import data from file ---------------------------------------------------
 
@@ -18,8 +16,11 @@ g_assemblages_bpg <-
 V(g_assemblages_bpg)$type <-
   bipartite_mapping(g_assemblages_bpg)$type
 
+g_assemblages_bpg_layout <-
+  g_assemblages_bpg %>% layout_as_bipartite()
+
 g_assemblages_bpg %>%
-  ggraph(layout = "bipartite") +
+  ggraph(layout = g_assemblages_bpg_layout) +
   geom_edge_link(color = "gray", alpha = 0.25) +
   geom_node_point(aes(color = type), size = 2) +
   scale_color_manual(
@@ -32,24 +33,24 @@ g_assemblages_bpg %>%
 
 # Create incidence matrix from bipartite graph ----------------------------
 
-g_assemblages_inc <- as_incidence_matrix(g_assemblages_bpg)
+g_assemblages_bpg_inc <- as_incidence_matrix(g_assemblages_bpg)
 
 # Project one-mode graphs igraph ------------------------------------------
 
 g_assemblages_proj <-
   bipartite_projection(g_assemblages_bpg, multiplicity = TRUE)
 
-g_assemblage_prov <- g_assemblages_proj$proj1
+g_assemblages_proj_prov <- g_assemblages_proj$proj1
 
-g_assemblage_artifact <- g_assemblages_proj$proj2
+g_assemblages_proj_artifact <- g_assemblages_proj$proj2
 
-g_assemblage_prov %>%
+g_assemblages_proj_prov %>%
   ggraph(layout = "fr") +
   geom_edge_link(color = "gray", aes(alpha = weight)) +
   geom_node_point(color = "green", size = 2) +
   ggtitle("Network of Proveniences")
 
-g_assemblage_artifact %>%
+g_assemblages_proj_artifact %>%
   ggraph(layout = "fr") +
   geom_edge_link(color = "gray", aes(alpha = weight)) +
   geom_node_point(color = "blue", size = 2) +
@@ -63,23 +64,46 @@ g_assemblage_artifact %>%
 overlap_coef <- function(x, y = NULL) {
   if (is.null(y))
     y <- x
+  
   t_mat <- t(x) %*% y
+  
   x_count <- apply(x, 2, function(xx)
     sum(xx != 0))
+  
   y_count <- apply(y, 2, function(yy)
     sum(yy != 0))
+  
   t_mat_pmin <- outer(x_count, y_count, FUN = pmin)
+  
   res <- t_mat / t_mat_pmin
+  
   if (is.null(y)) {
     diag(res) <- 1L
   }
+  
   dimnames(res) <- list(colnames(x), colnames(y))
+  
   return(res)
 }
 
-prov_adj_ssoc <- overlap_coef(t(g_assemblages_inc))
+prov_adj_ssoc <- overlap_coef(t(g_assemblages_bpg_inc))
+diag(prov_adj_ssoc) <- 0
 
-artifact_adj_ssoc <- overlap_coef(g_assemblages_inc)
+prov_ssoc_vals <- prov_adj_ssoc[lower.tri(prov_adj_ssoc)]
+
+ggplot(data = data.frame(x = c(prov_ssoc_vals)), aes(x = x)) +
+  geom_density(color = "green") +
+  ggtitle("Distribution of Szymkiewicz-Simpson Similarity for Proveniences")
+
+artifact_adj_ssoc <- overlap_coef(g_assemblages_bpg_inc)
+diag(artifact_adj_ssoc) <- 0
+
+artifact_ssoc_vals <-
+  artifact_adj_ssoc[lower.tri(artifact_adj_ssoc)]
+
+ggplot(data = data.frame(x = c(artifact_ssoc_vals)), aes(x = x)) +
+  geom_density(color = "blue") +
+  ggtitle("Distribution of Szymkiewicz-Simpson Similarity for Artifacts")
 
 
 # Project one-mode graphs, Sorenson-Dice -----------------------------------
@@ -87,19 +111,111 @@ artifact_adj_ssoc <- overlap_coef(g_assemblages_inc)
 soren_dice_sim <- function(x, y = NULL) {
   if (is.null(y))
     y <- x
-  t_mat <- (t(x) %*% y)
+  
+  t_mat <- t(x) %*% y
+  
   x_count <- apply(x, 2, function(xx)
     sum(xx != 0))
+  
   y_count <- apply(y, 2, function(yy)
     sum(yy != 0))
-  res <- (2 * t_mat) / (x_count + y_count)
+  
+  t_mat_sum <- outer(x_count, y_count, FUN = "+")
+  
+  res <- (2 * t_mat) / t_mat_sum
+  
   if (is.null(y)) {
     diag(res) <- 1L
   }
+  
   dimnames(res) <- list(colnames(x), colnames(y))
   return(res)
 }
 
-prov_adj_sdsim <- soren_dice_sim(t(g_assemblages_inc))
+prov_adj_sdsim <- soren_dice_sim(t(g_assemblages_bpg_inc))
+diag(prov_adj_sdsim) <- 0
 
-artifact_adj_sdsim <- soren_dice_sim(g_assemblages_inc)
+prov_sdsim_val <-
+  prov_adj_sdsim[lower.tri(prov_adj_sdsim)]
+
+ggplot(data = data.frame(x = prov_sdsim_val), aes(x = x)) +
+  geom_density(color = "green") +
+  ggtitle("Distribution of Sorenson-Dice Similarity for Provenience")
+
+artifact_adj_sdsim <- soren_dice_sim(g_assemblages_bpg_inc)
+diag(artifact_adj_sdsim) <- 0
+
+artifact_sdsim_val <-
+  artifact_adj_sdsim[lower.tri(artifact_adj_sdsim)]
+
+ggplot(data = data.frame(x = artifact_sdsim_val), aes(x = x)) +
+  geom_density(color = "blue") +
+  ggtitle("Distribution of Sorenson-Dice Similarity for Artifacts")
+
+
+# Project one-mode graphs, Jaccard ----------------------------------------
+
+jaccard_sim <- function(x, y = NULL) {
+  if (is.null(y))
+    y <- x
+  
+  t_mat <- t(x) %*% y
+  
+  x_count <- apply(x, 2, function(xx)
+    sum(xx != 0))
+  
+  y_count <- apply(y, 2, function(yy)
+    sum(yy != 0))
+  
+  t_mat_sum <- outer(x_count, y_count, FUN = "+")
+  
+  res <- t_mat / (t_mat_sum - t_mat)
+  
+  if (is.null(y)) {
+    diag(res) <- 1L
+  }
+  
+  dimnames(res) <- list(colnames(x), colnames(y))
+  
+  return(res)
+}
+
+prov_adj_jacc <- jaccard_sim(t(g_assemblages_bpg_inc))
+diag(prov_adj_jacc) <- 0
+
+prov_jacc_val <-
+  prov_adj_jacc[lower.tri(prov_adj_jacc)]
+
+ggplot(data = data.frame(x = prov_jacc_val), aes(x = x)) +
+  geom_density(color = "green") +
+  ggtitle("Distribution of Jaccard Similarity for Provenience")
+
+artifact_adj_jacc <- jaccard_sim(g_assemblages_bpg_inc)
+diag(artifact_adj_jacc) <- 0
+
+artifact_jacc_val <-
+  artifact_adj_jacc[lower.tri(artifact_adj_jacc)]
+
+ggplot(data = data.frame(x = artifact_jacc_val), aes(x = x)) +
+  geom_density(color = "blue") +
+  ggtitle("Distribution of Jaccard Similarity for Artifacts")
+
+
+# TOM Adjacency -----------------------------------------------------------
+
+tom_adjacency_sim <- function(adj_mat) {
+  l_mat <- adj_mat %*% t(adj_mat)
+  
+  k_row <- rowSums(adj_mat)
+  k_col <- colSums(adj_mat)
+  k_min <- outer(k_row, k_col, FUN = pmin)
+  
+  tom_adj <- (l_mat + adj_mat) / (k_min + 1 - adj_mat)
+  
+  diag(tom_adj) <- 0
+  
+  dimnames(tom_adj) <- list(colnames(adj_mat), colnames(adj_mat))
+  
+  return(tom_adj)
+}
+
